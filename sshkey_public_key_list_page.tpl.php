e3c62f0170b68ccbda3744ca3d21b53e63ab8e21
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2010-03-04 09:49:11 +0100

    Initial commit

---
+++ sshkey_public_key_list_page.tpl.php
@@ -0,0 +1,10 @@
+<div id="sshkey-public-key-wrapper">
+  <div class="public-key-list-table">
+    <?php print $table; ?>
+  </div>
+  <hr />
+  <div class="add-public-key-form">
+    <h3><?php print t('Add a public key'); ?></h3>
+    <?php print $form; ?>
+  </div>
+</div>
